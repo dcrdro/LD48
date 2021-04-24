21efdb44102b33ef509fcffc0f0047d3088b1339
Ruslan <russ944@gmail.com>
2021-04-24 15:55:38 +0300

    Imported sample structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using OpachaTemplate.Core;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace OpachaTemplate.Core
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
