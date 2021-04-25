ed2c58090ae8fcb7a957457300e86d91aeb5a6e1
BodaMat <bomtvv@gmail.com>
2021-04-25 19:08:51 +0300

    connect animator to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,35 @@ using UnityEngine;
     public class Player : MonoBehaviour
     {
         public float moveSpeed = 1f;
-        
+        public Animator animator;
+
+        private Vector3 velocity;
+        public bool facingRight = true;
+
         void Update()
         {
             float hor = Input.GetAxis("Horizontal");
 
-            transform.position += Vector3.right * hor * moveSpeed * Time.deltaTime;
+            velocity = Vector3.right * hor;
+            animator.SetFloat("Velocity", Mathf.Abs(velocity.x));
+            transform.position += velocity * (moveSpeed * Time.deltaTime);
+
+            if (velocity.x < 0 && facingRight)
+            {
+                facingRight = false;
+                Flip();
+            }
+            else if (velocity.x > 0 && !facingRight)
+            {
+                facingRight = true;
+                Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
         }
     }
