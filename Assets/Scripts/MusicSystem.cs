c9c6a78e07bf59d059aa5db13ff7fbf31f9b0209
BodaMat <bomtvv@gmail.com>
2021-04-25 15:50:05 +0300

    add music system

---
+++ Assets/Scripts/MusicSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MusicSystem : MonoBehaviour
+{
+    public enum environmentMusic
+    {
+        Peaceful,
+        Depretion
+    }
+    
+    [Serializable]
+    public struct BGMusic {
+        public environmentMusic environment;
+        public AudioClip audio;
+    }
+
+    public BGMusic[] musics;
+    public AudioSource[] musicSources = new AudioSource[2];
+    public bool playOnStart = true;
+    
+    private float time = 0;
+
+    private void Start()
+    {
+        if (playOnStart) changeEnvironment(environmentMusic.Peaceful);
+    }
+
+    public void changeEnvironment(environmentMusic environment, float duration = 5f)
+    {
+        AudioClip changeToMusic = null;
+        foreach (var music in musics)
+        {
+            if (music.environment == environment)
+            {
+                changeToMusic = music.audio;
+            }
+        }
+        
+        if (changeToMusic) CrossFade(changeToMusic, duration);
+    }
+
+    void CrossFade(AudioClip to, float duration)
+    {
+        AudioSource activeSource = musicSources[0].isPlaying ? musicSources[0] : musicSources[1];
+        AudioSource extraSource = musicSources[0].isPlaying ? musicSources[1] : musicSources[0];
+        
+        DOTween.To(() => activeSource.volume, vol => activeSource.volume = vol, 0f, duration).OnComplete(() => activeSource.Stop());
+        extraSource.clip = to;
+        extraSource.volume = 0f;
+        extraSource.Play();
+        DOTween.To(() => extraSource.volume, vol => extraSource.volume = vol, 1f, duration);
+    }
+}
