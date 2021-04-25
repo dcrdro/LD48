fcd73484e643279b616452e31bb3adca3a048990
BodaMat <bomtvv@gmail.com>
2021-04-25 19:12:39 +0300

    add Audio System

---
+++ Assets/Scripts/AudioSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class AudioSystem : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public void PlaySound(AudioClip clip)
+    {
+        if (!musicSource.isPlaying)
+        {
+            musicSource.clip = clip;
+            musicSource.Play();
+        }
+    }
+}
