9df8778f044fdf36d6155ed34ab7e5da9d8f0d3a
BodaMat <bomtvv@gmail.com>
2021-04-27 01:09:26 +0300

    configure dialogue

--- Assets/Scripts/Dialogues/DialogID.cs
+++ Assets/Scripts/Dialogues/DialogID.cs
@@ -2,8 +2,15 @@
 {
     public enum DialogID
     {
-        npc_1 = 0,
-        npc_2,
-        wake_up
+        cut_doll = 0,
+        cut_mouse,
+        npc,
+        openCloth,
+        seeMohyly,
+        transformation_1,
+        transformation_2,
+        transformation_3,
+        transformation_4,
+        wakeUp
     }
 }
