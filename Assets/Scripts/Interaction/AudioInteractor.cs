ad8ae8db3917ea7d3d1d810ca7ad72f79b6592c2
BodaMat <bomtvv@gmail.com>
2021-04-26 22:55:54 +0300

    configure npc

---
+++ Assets/Scripts/Interaction/AudioInteractor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AudioInteractor : InteractorBase
+{
+    public AudioClip sound;
+    
+    public override void OnInteract()
+    {
+        AudioSystem.Instance.PlaySound(sound);
+    }
+
+    public override string Name => "";
+}
--- Assets/Scripts/Interaction/RotateInteractor.cs
+++ Assets/Scripts/Interaction/RotateInteractor.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using DG.Tweening;
 
-    public class RotateInteractor : InteractorBase
+public class RotateInteractor : InteractorBase
+{
+    public Transform target;
+    public Vector3 rotation;
+    public float time;
+    
+    public override void OnInteract()
     {
-        public Transform target;
-        public Vector3 rotation;
-        public float time;
-        
-        public override void OnInteract()
-        {
-            target.DORotate(rotation, time).SetRelative();
-        }
-
-        public override string Name => "";
+        target.DORotate(rotation, time).SetRelative();
     }
+
+    public override string Name => "";
+}
